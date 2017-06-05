2940d4296ac6230435b1545f9ffe356f06e7f182
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-05 20:05:25 +0200

    Implemented db_update - resolves #6

---
+++ db_update.js
@@ -0,0 +1,9 @@
+var Ouch = require('ouch-stream')
+module.exports = (db) => new Ouch(db).merge((message, doc) => {
+    if (doc){
+        Object.assign(doc.files,message.doc.files);
+        return doc;
+    } else {
+        return message.doc;
+    }
+})
