d36e8b5cee93510c7878d6c0243e40afb204310d
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-11 19:47:06 +0200

    Test for multiple instances of file

--- test/flow.js
+++ test/flow.js
@@ -10,16 +10,30 @@ describe('flow', () => {
       adapter: 'memory'
     })
     mock({
-      '/source/empty': ''
+      '/source/empty': '',
+      '/source/empty2': ''
     })
   })
   it('Should add file to db', (done) => {
     flow({
-      paths: '/source/**'
+      paths: '/source/empty'
     }, db).then(() => db.get('d41d8cd98f00b204e9800998ecf8427e')).then((doc) => {
       expect(doc).to.containSubset({
         'files': {
           '/source/empty': 'PRESENT'
+        }
+      })
+      done()
+    }).catch(done)
+  })
+  it('Should file identical files under the same document', (done) => {
+    flow({
+      paths: ['/source/empty', '/source/empty2']
+    }, db).then(() => db.get('d41d8cd98f00b204e9800998ecf8427e')).then((doc) => {
+      expect(doc).to.containSubset({
+        'files': {
+          '/source/empty': 'PRESENT',
+          '/source/empty2': 'PRESENT'
         }
       })
       done()
