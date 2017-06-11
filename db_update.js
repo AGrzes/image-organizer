77a0dba896308304d8455e4a8b2ba175c36dcaa8
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-11 19:43:08 +0200

    Added logging

--- db_update.js
+++ db_update.js
@@ -1,5 +1,7 @@
+var debug = require('debug')('db_update')
 var Ouch = require('ouch-stream')
 module.exports = (db) => new Ouch(db).merge((message, doc) => {
+  debug(`Message ${message.doc} Doc ${doc}`)
   if (doc) {
     Object.assign(doc.files, message.doc.files)
     return doc
