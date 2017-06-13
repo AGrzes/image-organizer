92603752e4c03952428343e15f4bf07a3df8960a
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-13 20:17:46 +0200

    Implemented setting status in link

--- link.js
+++ link.js
@@ -2,7 +2,12 @@ var fs = require('fs')
 var fsOp = require('./fs_op')
 module.exports = (target) => fsOp(target, (sourceExist, targetExist, message, destination, cb) => {
   if (!sourceExist && targetExist) {
-    fs.symlink(destination, message.file, cb)
+    fs.symlink(destination, message.file, (err) => {
+      if (!err) {
+        message.status = 'LINK'
+      }
+      cb(err)
+    })
   } else {
     cb()
   }
