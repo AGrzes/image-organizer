14dedbe8be822f28c20eb0898a6f6889ccc47b36
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-13 21:33:32 +0200

    Added logging

--- filter_images.js
+++ filter_images.js
@@ -1,6 +1,8 @@
 var miss = require('mississippi')
 var minimatch = require('minimatch')
+var debug = require('debug')('filter_images')
 module.exports = (patterns) => miss.through.obj((message, encoding, cb) => {
+  debug(`mime ${message.exif.MIMEType} patterns ${patterns}`)
   if (patterns.some((pattern) => minimatch(message.exif.MIMEType, pattern))) {
     cb(null, message)
   } else {
