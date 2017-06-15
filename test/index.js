ab0350ea5afde4a30d643cfc98e932a324649055
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-15 16:31:38 +0200

    First full command line test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 var childProcess = require('child_process')
 var expect = require('chai').expect
 var PouchDB = require('pouchdb-core')
+var os = require('os')
+var fs = require('fs')
+var path = require('path')
 PouchDB.plugin(require('pouchdb-adapter-http'))
 describe('index', () => {
   var pouchServer
@@ -14,19 +17,32 @@ describe('index', () => {
     })
   })
   var db
+  var src
+  var dst
+  var machine = os.hostname()
   beforeEach((done) => {
-    db = new PouchDB('http://localhost:3000/test_db')
-    var counter = 10
-    var wait = () => {
-      db.allDocs().then(() => done()).catch((err) => {
-        if (err.code === 'ECONNREFUSED' && counter-- > 0) {
-          setTimeout(wait, 100)
-        } else {
-          done(err)
+    var tmpdir = path.join(os.tmpdir(), (new Date().getTime()).toString())
+    fs.mkdirSync(tmpdir)
+    src = path.join(tmpdir, 'src')
+    dst = path.join(tmpdir, 'dst')
+    childProcess.spawn('cp', ['-r', 'test/src', src]).on('exit', (code) => {
+      if (code === 0) {
+        db = new PouchDB('http://localhost:3000/test_db')
+        var counter = 20
+        var wait = () => {
+          db.allDocs().then(() => done()).catch((err) => {
+            if (err.code === 'ECONNREFUSED' && counter-- > 0) {
+              setTimeout(wait, 100)
+            } else {
+              done(err)
+            }
+          })
         }
-      })
-    }
-    wait()
+        wait()
+      } else {
+        done(new Error(`Source directory copy failed 'cp', ['-r', 'test/src', ${src}]`))
+      }
+    })
   })
   afterEach((done) => {
     db.destroy().then(() => done()).catch(done)
@@ -35,6 +51,27 @@ describe('index', () => {
     childProcess.fork('./index', []).on('exit', (code) => {
       expect(code).not.to.be.equal(0)
       done()
+    })
+  })
+
+  it('Should scan source directory', (done) => {
+    childProcess.fork('./index', ['-a', 'http://localhost:3000/test_db', '-p', path.join(src, '**'), '-t', dst]).on('exit', (code) => {
+      db.allDocs({
+        include_docs: true
+      }).then((docs) => {
+        expect(docs).to.be.containSubset({
+          rows: [{
+            doc: {
+              files: {
+                [machine]: {
+                  [path.join(src, 'file1.jpg')]: 'PRESENT'
+                }
+              }
+            }
+          }]
+        })
+        done()
+      }).catch(done)
     })
   })
 })
