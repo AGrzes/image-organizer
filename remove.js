e476a012b6b3b956ec52dbf0127cfbe6072fc09f
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-16 12:04:32 +0200

    Tests for remove

--- remove.js
+++ remove.js
@@ -1,10 +1,10 @@
-var fse = require('fs-extra')
+var fs = require('fs')
 var fsOp = require('./fs_op')
 var debug = require('debug')('remove')
 module.exports = (target) => fsOp(target, (sourceExist, targetExist, message, destination, cb) => {
   if (sourceExist && targetExist) {
     debug(`removing ${message.file}`)
-    fse.unlink(message.file, (err) => {
+    fs.unlink(message.file, (err) => {
       if (!err) {
         message.status = 'ABSENT'
       }
