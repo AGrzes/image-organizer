9e01ed4ea7d90d8068248c8b8b287d7c4020ec70
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-06-26 21:03:26 +0200

    Setting timestamp

--- src/file/copy.js
+++ src/file/copy.js
@@ -1,8 +1,16 @@
 var fse = require('fs-extra')
 var fsOp = require('./fs_op')
+var moment = require('moment')
 module.exports = (target) => fsOp(target, (sourceExist, targetExist, message, destination, cb) => {
   if (sourceExist && !targetExist) {
-    fse.copy(message.file, destination, cb)
+    fse.copy(message.file, destination, (error) => {
+      if (error) {
+        cb(error)
+      } else {
+        var date = moment(message.exif.CreateDate, 'YYYY:MM:DD HH:mm:ss')
+        fse.utimes(destination, new Date(), date.toDate(), cb)
+      }
+    })
   } else {
     cb()
   }
--- test/file/copy.js
+++ test/file/copy.js
@@ -1,8 +1,9 @@
 var mock = require('mock-fs')
-var expect = require('chai').expect
+var expect = require('chai').use(require('chai-datetime')).expect
 var copy = require('../../src/file/copy')
 var StreamTest = require('streamtest')
 var fs = require('fs')
+var moment = require('moment')
 describe('copy', () => {
   beforeEach(() => {
     mock({
@@ -20,6 +21,19 @@ describe('copy', () => {
         }]).pipe(copy('/')).pipe(StreamTest[version].toObjects((error) => {
           expect(fs.existsSync('/target')).to.be.true
           expect(fs.readFileSync('/target', 'UTF-8')).to.be.equal('source')
+          done(error)
+        }))
+      })
+      it('should set the file modification time to creation time', function (done) {
+        StreamTest[version].fromObjects([{
+          exif: {
+            CreateDate: '2000-12-21'
+          },
+          file: '/source',
+          target: 'target'
+        }]).pipe(copy('/')).pipe(StreamTest[version].toObjects((error) => {
+          expect(fs.existsSync('/target')).to.be.true
+          expect(fs.statSync('/target').mtime).to.be.equalTime(moment('2000-12-21').toDate())
           done(error)
         }))
       })
