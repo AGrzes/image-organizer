ec04e99d8a0516137cc1e8cf9a75396106e0a5d4
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-07-01 21:25:27 +0200

    Disabled closing log stream

--- test/control/report.js
+++ test/control/report.js
@@ -1,36 +1,49 @@
 var expect = require('chai').expect
 var report = require('../../src/control/report')
 var devNull = require('dev-null')
+var miss = require('mississippi')
 var StreamTest = require('streamtest')
-describe('set_target', () => {
+describe('report', () => {
   StreamTest.versions.forEach(function (version) {
     describe('for ' + version + ' streams', function () {
       it('should report file', (done) => {
-        StreamTest[version].toText()
-        StreamTest[version].fromObjects([{
-          file: 'file',
-          machine: 'machine'
-        }]).pipe(report(StreamTest[version].toText((error, text) => {
+        var testStream = StreamTest[version].toText((error, text) => {
           expect(text).to.be.equals('Processing machine:file\n')
           done(error)
-        }))).pipe(devNull({
+        })
+        miss.pipe(StreamTest[version].fromObjects([{
+          file: 'file',
+          machine: 'machine'
+        }]), report(testStream), devNull({
           objectMode: true
-        }))
+        }), (error) => {
+          if (error) {
+            done(error)
+          } else {
+            testStream.end()
+          }
+        })
       })
       it('should report every file', (done) => {
-        StreamTest[version].toText()
-        StreamTest[version].fromObjects([{
+        var testStream = StreamTest[version].toText((error, text) => {
+          expect(text).to.be.equals('Processing machine:file1\nProcessing machine:file2\n')
+          done(error)
+        })
+        miss.pipe(StreamTest[version].fromObjects([{
           file: 'file1',
           machine: 'machine'
         }, {
           file: 'file2',
           machine: 'machine'
-        }]).pipe(report(StreamTest[version].toText((error, text) => {
-          expect(text).to.be.equals('Processing machine:file1\nProcessing machine:file2\n')
-          done(error)
-        }))).pipe(devNull({
+        }]), report(testStream), devNull({
           objectMode: true
-        }))
+        }), (error) => {
+          if (error) {
+            done(error)
+          } else {
+            testStream.end()
+          }
+        })
       })
     })
   })
